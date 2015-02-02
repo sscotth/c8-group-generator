d73262552c5ece5ecfdcb26d974e2769d8b720f2
Scott <scott@sscotth.io>
2015-02-02 10:57:02 -0600

    domready and cleanup scope

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
-var $input = $('input'),
-    $ul    = $('ul'),
-    url    = 'https://yspuku7qvh9u4cr3.firebaseio.com/.json';
+var $input,
+    $ul,
+    url = 'https://yspuku7qvh9u4cr3.firebaseio.com/.json';
 
-$input.change(getUpdateAndSplit);
-document.addEventListener('DOMContentLoaded', getUpdateAndSplit);
+$(document).ready(init);
+
+function init() {
+  $input = $('input'),
+  $ul    = $('ul');
+
+  $input.change(getUpdateAndSplit);
+  getUpdateAndSplit();
+}
 
 function getUpdateAndSplit(){
   var count = $input.val();
